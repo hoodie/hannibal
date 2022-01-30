sunli829/xactor

test: modify example to demonstrate the missing functionality

examples/supervisor_clear_interval.rs
@@ -1,30 +1,20 @@
-use std::time::{Duration, Instant};
+use std::time::Duration;
 use xactor::{message, Actor, Context, Handler};
 
 #[derive(Debug)]
-pub struct PingTimer {
-    last_ping: Instant,
-}
-
-impl Default for PingTimer {
-    fn default() -> Self {
-        PingTimer {
-            last_ping: Instant::now(),
-        }
-    }
-}
+pub struct PingTimer;
 
 #[async_trait::async_trait]
 impl Actor for PingTimer {
     async fn started(&mut self, ctx: &mut Context<Self>) -> xactor::Result<()> {
-        println!("PingTimer:: started()");
-        ctx.send_interval(Ping, Duration::from_millis(1000));
+        println!("PingTimer :: started()");
+        ctx.send_interval(Ping, Duration::from_millis(300));
         Ok(())
     }
 
     /// Called after an actor is stopped.
     async fn stopped(&mut self, _: &mut Context<Self>) {
-        println!("PingTimer:: stopped()");
+        println!("PingTimer :: stopped()");
     }
 }
 
@@ -34,22 +24,29 @@ struct Ping;
 
 #[async_trait::async_trait]
 impl Handler<Ping> for PingTimer {
-    async fn handle(&mut self, ctx: &mut Context<Self>, _msg: Ping) {
-        let now = Instant::now();
-        let delta = (now - self.last_ping).as_millis();
-        self.last_ping = now;
-        println!("PingTimer:: Ping {} {:?}", ctx.actor_id(), delta);
+    async fn handle(&mut self, _: &mut Context<Self>, _msg: Ping) {
+        println!("PingTimer :: Ping");
     }
 }
 #[message]
-struct Halt;
+struct Restart;
 
 #[async_trait::async_trait]
-impl Handler<Halt> for PingTimer {
-    async fn handle(&mut self, ctx: &mut Context<Self>, _msg: Halt) {
-        println!("PingTimer:: received Halt");
+impl Handler<Restart> for PingTimer {
+    async fn handle(&mut self, ctx: &mut Context<Self>, _msg: Restart) {
+        println!("PingTimer :: received restart");
         ctx.stop(None);
-        println!("PingTimer:: stopped");
+    }
+}
+
+#[message]
+struct Shutdown;
+
+#[async_trait::async_trait]
+impl Handler<Shutdown> for PingTimer {
+    async fn handle(&mut self, ctx: &mut Context<Self>, _msg: Shutdown) {
+        println!("PingTimer :: received Shutdown");
+        // ctx.stop_supervisor(None);
     }
 }
 
@@ -59,29 +56,48 @@ struct Panic;
 #[async_trait::async_trait]
 impl Handler<Panic> for PingTimer {
     async fn handle(&mut self, _: &mut Context<Self>, _msg: Panic) {
-        println!("PingTimer:: received Panic");
-        panic!("intentional panic");
+        println!("PingTimer :: received Panic");
+        panic!("intentional panic: this should not occur");
     }
 }
 
 #[xactor::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let service_supervisor = xactor::Supervisor::start(PingTimer::default).await?;
+    let service_supervisor = xactor::Supervisor::start(|| PingTimer).await?;
     let service_addr = service_supervisor.clone();
+    let service_addr2 = service_supervisor.clone();
 
     let supervisor_task = xactor::spawn(async {
         service_supervisor.wait_for_stop().await;
     });
 
-    let send_halt = async {
-        xactor::sleep(Duration::from_millis(5_200)).await;
-        println!("  main  :: sending Halt");
-        service_addr.send(Halt).unwrap();
+    let stop_actor = async {
+        xactor::sleep(Duration::from_millis(2_000)).await;
+        println!("   main   :: sending Restart");
+        service_addr.send(Restart).unwrap();
     };
 
-    let _ = futures::join!(supervisor_task, send_halt);
-    // run this to see that the interval is not properly stopped if the ctx is stopped
-    // futures::join!(supervisor_task, send_panic); // there is no panic recovery
+    let stop_supervisor = async move {
+        xactor::sleep(Duration::from_millis(3_000)).await;
+        println!("   main   :: sending Shutdown");
+        service_addr2.send(Shutdown).unwrap();
+    };
+
+    let send_panic = async {
+        xactor::sleep(Duration::from_millis(5_000)).await;
+        println!("   main   :: sending Panic after stop");
+        if let Err(error) = service_addr.send(Panic) {
+            println!("    ok    :: cannot send after halting, this is very much expected");
+            println!("             Failing with \"{}\"", error);
+        }
+    };
+
+    futures::join!(
+        supervisor_task,
+        stop_actor,
+        stop_supervisor,
+        send_panic, // there is no panic recovery
+    );
 
     Ok(())
 }

hoodie/hannibal

examples/supervisor_clear_interval.rs
@@ -1,30 +1,20 @@
-use std::time::{Duration, Instant};
 use hannibal::{message, Actor, Context, Handler};
+use std::time::Duration;
 
 #[derive(Debug)]
-pub struct PingTimer {
-    last_ping: Instant,
-}
-
-impl Default for PingTimer {
-    fn default() -> Self {
-        PingTimer {
-            last_ping: Instant::now(),
-        }
-    }
-}
+pub struct PingTimer;
 
 #[async_trait::async_trait]
 impl Actor for PingTimer {
     async fn started(&mut self, ctx: &mut Context<Self>) -> hannibal::Result<()> {
-        println!("PingTimer:: started()");
-        ctx.send_interval(Ping, Duration::from_millis(1000));
+        println!("PingTimer :: started()");
+        ctx.send_interval(Ping, Duration::from_millis(300));
         Ok(())
     }
 
     /// Called after an actor is stopped.
     async fn stopped(&mut self, _: &mut Context<Self>) {
-        println!("PingTimer:: stopped()");
+        println!("PingTimer :: stopped()");
     }
 }
 
@@ -34,22 +24,29 @@ struct Ping;
 
 #[async_trait::async_trait]
 impl Handler<Ping> for PingTimer {
-    async fn handle(&mut self, ctx: &mut Context<Self>, _msg: Ping) {
-        let now = Instant::now();
-        let delta = (now - self.last_ping).as_millis();
-        self.last_ping = now;
-        println!("PingTimer:: Ping {} {:?}", ctx.actor_id(), delta);
+    async fn handle(&mut self, _: &mut Context<Self>, _msg: Ping) {
+        println!("PingTimer :: Ping");
     }
 }
 #[message]
-struct Halt;
+struct Restart;
 
 #[async_trait::async_trait]
-impl Handler<Halt> for PingTimer {
-    async fn handle(&mut self, ctx: &mut Context<Self>, _msg: Halt) {
-        println!("PingTimer:: received Halt");
+impl Handler<Restart> for PingTimer {
+    async fn handle(&mut self, ctx: &mut Context<Self>, _msg: Restart) {
+        println!("PingTimer :: received restart");
         ctx.stop(None);
-        println!("PingTimer:: stopped");
+    }
+}
+
+#[message]
+struct Shutdown;
+
+#[async_trait::async_trait]
+impl Handler<Shutdown> for PingTimer {
+    async fn handle(&mut self, ctx: &mut Context<Self>, _msg: Shutdown) {
+        println!("PingTimer :: received Shutdown");
+        // ctx.stop_supervisor(None);
     }
 }
 
@@ -59,29 +56,48 @@ struct Panic;
 #[async_trait::async_trait]
 impl Handler<Panic> for PingTimer {
     async fn handle(&mut self, _: &mut Context<Self>, _msg: Panic) {
-        println!("PingTimer:: received Panic");
-        panic!("intentional panic");
+        println!("PingTimer :: received Panic");
+        panic!("intentional panic: this should not occur");
     }
 }
 
 #[hannibal::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let service_supervisor = hannibal::Supervisor::start(PingTimer::default).await?;
+    let service_supervisor = hannibal::Supervisor::start(|| PingTimer).await?;
     let service_addr = service_supervisor.clone();
+    let service_addr2 = service_supervisor.clone();
 
     let supervisor_task = hannibal::spawn(async {
         service_supervisor.wait_for_stop().await;
     });
 
-    let send_halt = async {
-        hannibal::sleep(Duration::from_millis(5_200)).await;
-        println!("  main  :: sending Halt");
-        service_addr.send(Halt).unwrap();
+    let stop_actor = async {
+        hannibal::sleep(Duration::from_millis(2_000)).await;
+        println!("   main   :: sending Restart");
+        service_addr.send(Restart).unwrap();
     };
 
-    let _ = futures::join!(supervisor_task, send_halt);
-    // run this to see that the interval is not properly stopped if the ctx is stopped
-    // futures::join!(supervisor_task, send_panic); // there is no panic recovery
+    let stop_supervisor = async move {
+        hannibal::sleep(Duration::from_millis(3_000)).await;
+        println!("   main   :: sending Shutdown");
+        service_addr2.send(Shutdown).unwrap();
+    };
+
+    let send_panic = async {
+        hannibal::sleep(Duration::from_millis(5_000)).await;
+        println!("   main   :: sending Panic after stop");
+        if let Err(error) = service_addr.send(Panic) {
+            println!("    ok    :: cannot send after halting, this is very much expected");
+            println!("             Failing with \"{}\"", error);
+        }
+    };
+
+    futures::join!(
+        supervisor_task,
+        stop_actor,
+        stop_supervisor,
+        send_panic, // there is no panic recovery
+    );
 
     Ok(())
 }