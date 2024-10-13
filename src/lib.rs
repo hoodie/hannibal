8ce4b657cc5313d3d460b280eaa34497fd743405
Hendrik Sollich <hendrik@hoodie.de>
2024-10-13 20:12:25 +0200

    style: move lints into Cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,3 @@
-#![allow(clippy::type_complexity)]
-#![warn(clippy::doc_markdown)]
-#![deny(trivial_numeric_casts, unsafe_code, unused_import_braces)]
-#![warn(
-    clippy::dbg_macro,
-    clippy::doc_markdown,
-    clippy::indexing_slicing,
-    clippy::redundant_closure_for_method_calls,
-
-    clippy::map_clone,
-    clippy::correctness,
-    clippy::style,
-    clippy::manual_filter_map,
-    clippy::useless_format,
-    clippy::clone_on_ref_ptr,
-
-    clippy::complexity,
-    clippy::too_many_arguments,
-    clippy::needless_collect,
-    clippy::expect_used,
-    clippy::unwrap_used,
-    clippy::cast_lossless,
-    clippy::cast_possible_truncation,
-    clippy::missing_panics_doc,
-    clippy::missing_const_for_fn,
-    clippy::redundant_clone,
-
-    missing_copy_implementations,
-    // missing_debug_implementations,
-    // missing_docs,
-    trivial_casts,
-    trivial_numeric_casts,
-    unsafe_code,
-    unstable_features,
-    unused_import_braces,
-)]
 #![cfg_attr(test, allow(clippy::unwrap_used))]
 
 mod actor;
