7ef2536e6380fe29ee126838145e82ee54f455cb
Hendrik Sollich <hendrik@hoodie.de>
2024-10-20 18:06:22 +0200

    docs: add simple service example

---
+++ examples/simple-service.rs
@@ -0,0 +1,50 @@
+use minibal::prelude::*;
+use std::collections::HashMap;
+
+#[derive(Debug, Default)]
+struct StorageService {
+    storage: HashMap<String, String>,
+}
+
+struct Store(&'static str, &'static str);
+
+impl Message for Store {
+    type Result = ();
+}
+
+struct Retrieve(&'static str);
+
+impl Message for Retrieve {
+    type Result = Option<String>;
+}
+
+impl Actor for StorageService {}
+impl Service for StorageService {}
+
+impl Handler<Store> for StorageService {
+    async fn handle(&mut self, _: &mut Context<Self>, Store(key, value): Store) {
+        self.storage.insert(key.to_string(), value.to_string());
+    }
+}
+
+impl Handler<Retrieve> for StorageService {
+    async fn handle(&mut self, _: &mut Context<Self>, Retrieve(key): Retrieve) -> Option<String> {
+        self.storage.get(key).cloned()
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    StorageService::from_registry()
+        .await
+        .send(Store("password", "hello world"))
+        .unwrap();
+
+    let result = StorageService::from_registry()
+        .await
+        .call(Retrieve("password"))
+        .await
+        .unwrap();
+
+    println!("retrieved: {:?}", result);
+}
