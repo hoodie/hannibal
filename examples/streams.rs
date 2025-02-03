f02d0c43e94a39c06654bd87bb104deb8ea5e144
Hendrik Sollich <hendrik@hoodie.de>
2025-02-03 20:27:31 +0100

    docs: add streams example

---
+++ examples/streams.rs
@@ -0,0 +1,34 @@
+use minibal::prelude::*;
+
+#[derive(Default, Actor)]
+struct FizzBuzzer(&'static str);
+
+impl StreamHandler<i32> for FizzBuzzer {
+    async fn handle(&mut self, _ctx: &mut Context<Self>, msg: i32) {
+        match (msg % 3 == 0, msg % 5 == 0) {
+            (true, true) => {
+                self.0 = "fizzbuzz";
+                println!("{msg} -> {inner}", inner = self.0, msg = msg);
+            }
+            (true, false) => {
+                self.0 = "fizz";
+                println!("{msg} -> {inner}", inner = self.0, msg = msg);
+            }
+            (false, true) => {
+                self.0 = "buzz";
+                println!("{msg} -> {inner}", inner = self.0, msg = msg);
+            }
+            _ => {}
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let num_stream = futures::stream::iter(1..30);
+    let addr = minibal::build(FizzBuzzer("Caesar"))
+        .on_stream(num_stream)
+        .spawn();
+
+    addr.await.unwrap();
+}
